b69202f122aadef72dd0a84c60d4feabfd603ae0
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2025-02-27 11:05:04 +0500

    feat: develop photos,users pages,  router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './pages/posts/posts.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { PhotosComponent } from './pages/photos/photos.component';
 import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   { path: 'posts', component: PostsComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'photos', component: PhotosComponent },
   { path: 'users', component: UsersComponent },
-  { path: '', redirectTo: 'posts', pathMatch: 'full' },
-  { path: '**', redirectTo: 'posts' }
+  { path: '', redirectTo: 'posts', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/' } 
 ];
